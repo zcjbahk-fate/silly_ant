6bddb6b8842c3af696801fa4782276e8b3817d64
zcjbahk <2602007594@qq.com>
2026-08-23 22:34:27 +0800

    release: 2026-08-23 [桃色公寓]

--- work/角色卡/桃色公寓/脚本/变量结构.js
+++ work/角色卡/桃色公寓/脚本/变量结构.js
@@ -8,10 +8,11 @@ const TenantSchema = z
     与黑人性交次数: z.coerce.number().prefault(0),
     现在在做: z.string().prefault(""),
   })
+  .prefault({})
   .transform((data) => {
-    data.已上交租金 = Math.max(0, data.已上交租金);
-    data.榨精次数 = Math.max(0, data.榨精次数);
-    data.与黑人性交次数 = Math.max(0, data.与黑人性交次数);
+    data.已上交租金 = Math.max(0, data.已上交租金 || 0);
+    data.榨精次数 = Math.max(0, data.榨精次数 || 0);
+    data.与黑人性交次数 = Math.max(0, data.与黑人性交次数 || 0);
     return data;
   });
 
@@ -20,13 +21,15 @@ export const Schema = z.object({
   时间: z.string().prefault("2025年6月1日 08:00"),
   地点: z.string().prefault("欲都公寓 三楼办公室"),
   正在交互角色: z.string().prefault(""),
-  房东状态: z.object({
-    资金总额: z.coerce.number().prefault(10000),
-    地下室收藏品: z.array(z.string()).prefault([
-      "前租客谭婷婷用来抵债的原味粉色内裤",
-      "温知夏当生日礼物送给{{user}}的原味红底细跟高跟鞋",
-    ]),
-  }),
+  房东状态: z
+    .object({
+      资金总额: z.coerce.number().prefault(10000),
+      地下室收藏品: z.array(z.string()).prefault([
+        "前租客谭婷婷用来抵债的原味粉色内裤",
+        "温知夏当生日礼物送给{{user}}的原味红底细跟高跟鞋",
+      ]),
+    })
+    .prefault({}),
   房客状态: z
     .record(z.string().describe("女租客姓名"), TenantSchema)
     .prefault({}),
